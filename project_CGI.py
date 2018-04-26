4d221f08b3164532ec0f781c59b29885370b3cf1
Odysseas Tigas <OTIGAS@HOTMAIL.COM>
2018-04-26 22:01:23 +0300

    Add files via upload

---
+++ project_CGI.py
@@ -0,0 +1,7 @@
+
+
+# Max_overlapping_spacecrafts_timeslot-project_implemented_with_python_on_Linux_environment-/
+
+# Max overlapping spacecrafts timeslot project implemented with python on Linux environment. 
+
+# An interview project for a space industry related software company.
